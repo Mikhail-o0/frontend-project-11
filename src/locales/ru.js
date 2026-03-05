e1512c6acb10a6b12466caef25c0aa791a609cfc
root <root@Mikhail.localdomain>
2026-03-05 12:55:44 +0300

    feat: add i18n localization

---
+++ src/locales/ru.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    success: 'RSS успешно загружен',
+    feedsTitle: 'Фиды',
+    postsTitle: 'Посты',
+    viewButton: 'Просмотр',
+    errors: {
+      required: 'Не должно быть пустым',
+      url: 'Ссылка должна быть валидным URL',
+      notOneOf: 'RSS уже существует',
+      network: 'Ошибка сети',
+      parse: 'Ресурс не содержит валидный RSS',
+    },
+  },
+};
