24ab3b22dcad4a0bf6e50fa85964eee9eddd6030
root <root@Mikhail.localdomain>
2026-03-05 12:56:04 +0300

    feat: add RSS fetching and parsing

---
+++ src/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const PROXY_URL = 'https://allorigins.hexlet.app/get';
+
+const fetchRSS = (url) => {
+  const params = new URLSearchParams({
+    url,
+    disableCache: 'true',
+  });
+  
+  const fullUrl = `${PROXY_URL}?${params.toString()}`;
+  
+  return axios.get(fullUrl)
+    .then((response) => {
+      if (!response.data.contents) {
+        throw new Error('network');
+      }
+      return response.data.contents;
+    })
+    .catch((err) => {
+      throw new Error('network');
+    });
+};
+
+export default fetchRSS;
